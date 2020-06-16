5188dc4bdb42c82347e909db5044a7e3293b684c
Omar Aljazairy <omar@fedal.net>
2020-06-16 11:38:54 +0200

    first version of the script before refactoring

---
+++ main.py
@@ -0,0 +1,60 @@
+import firebase_admin
+from firebase_admin import auth
+import click
+
+@click.group()
+def main():
+    pass
+
+def print_style(text, fg='white', bg='black', blink=False, bold=True):
+    return click.echo(click.style((f'{text}'), fg=fg, bg=bg, blink=blink, bold=bold))
+
+@main.command()
+def users():
+    """ to get all users"""
+    page = auth.list_users()
+    for user in page.users:
+        user_email = user.email if hasattr(user, 'email') else None
+        user_displayName = user.displayName if hasattr(user, 'displayName') else None
+        # click.echo(click.style((f'{user.uid}, - , {user_email} - {user_displayName}'), fg='white', bg='black', blink=False, bold=False))
+        text = f'{user.uid}, - , {user_email} - {user_displayName}'
+        print_style(text)
+
+@main.command()
+@click.option('--uid', prompt='userId' ,help='firebase userid')
+def user(uid):
+    """get single user based on uid"""
+    print('uid received: %s' % (uid))
+    user = auth.get_user(uid)
+    
+    print('user: {0}'.format(user.__dict__))
+    for k,v in user.__dict__['_data'].items():
+        text = f'{k} -  {v}'
+        print_style(text)
+        # click.echo(click.style((f'{k}, - , {v}'), fg='white', bg='black', blink=False, bold=False))
+
+@main.command()
+@click.option('--id', prompt="select a function to execute:", type=click.IntRange(1,2))
+def menu(id):
+    """temporary menu to start the app"""
+    if id == 1:
+        return users()
+    elif id == 2:
+        return user()
+    else:
+        return 'unknown option'
+
+
+# user(sys.argv[1])
+if __name__ == '__main__':
+    # initialize firebase connection
+    default_app = firebase_admin.initialize_app()
+    funcs = ['users', 'user']
+    y = 0
+    for x in funcs:
+        y += 1
+        # click.echo(click.style((f'{y}, - , {x}'), fg='yellow', bg='black', blink=False, bold=True))
+        # text = str(y) + ' - ' + str(x)
+        text = f'[{y}] -  {x}'
+        print_style(text, fg='yellow', bold=True)
+    menu()
